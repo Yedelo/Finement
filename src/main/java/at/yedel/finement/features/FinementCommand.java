aecea9eed41c72a1a3076eab354d8336bd0820f7
Yedelo <158670400+Yedelo@users.noreply.github.com>
2024-10-27 00:17:42 -0400

    Added an information for showing color codes and privated unconfig fields

--- src/main/java/at/yedel/finement/features/FinementCommand.java
+++ src/main/java/at/yedel/finement/features/FinementCommand.java
@@ -4,8 +4,10 @@ package at.yedel.finement.features;
 
 import at.yedel.finement.config.FinementConfig;
 import cc.polyfrost.oneconfig.libs.universal.ChatColor;
+import cc.polyfrost.oneconfig.libs.universal.UChat;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
+import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommand;
 
 
 
@@ -14,5 +16,10 @@ public class FinementCommand {
 	@Main
 	private void main() {
 		FinementConfig.getInstance().openGui();
+	}
+
+	@SubCommand(description = "Shows color codes in chat.")
+	public void colors() {
+		UChat.chat("§00§11§22§33§44§55§66§77§88§99§aa§bb§cc§dd§ee§ff");
 	}
 }
