ef2cef8107bfc9cac3432f85270facacdcd44ce8
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-07-10 15:18:27 -0400

    Fix crash with ModifyArgs in interface inventory features

--- src/main/java/at/yedel/finement/mixins/MixinGuiContainer.java
+++ src/main/java/at/yedel/finement/mixins/MixinGuiContainer.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
@@ -24,11 +25,15 @@ public abstract class MixinGuiContainer {
 	@Shadow protected int guiTop;
 	@Unique private int finement$mouseX;
 	@Unique private int finement$mouseY;
+	@Unique private Slot finement$slotUnderMouse;
 
 	@Inject(method = "drawScreen", at = @At("HEAD"))
 	private void finement$storeMousePositions(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
 		this.finement$mouseX = mouseX;
 		this.finement$mouseY = mouseY;
+		if (FinementConfig.getInstance().enabled && FinementConfig.getInstance().snapItemRendering) {
+			finement$slotUnderMouse = getSlotUnderMouse();
+		}
 	}
 
 	@ModifyArgs(method = "drawScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiContainer;drawGradientRect(IIIIII)V"))
@@ -41,14 +46,14 @@ public abstract class MixinGuiContainer {
 		}
 	}
 
-	@ModifyArgs(method = "drawScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiContainer;drawItemStack(Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V"))
-	private void finement$snapItemRendering(Args args) {
-		if (FinementConfig.getInstance().enabled && FinementConfig.getInstance().snapItemRendering) {
-			Slot slotUnderMouse = getSlotUnderMouse();
-			if (slotUnderMouse != null) {
-				args.set(1, slotUnderMouse.xDisplayPosition);
-				args.set(2, slotUnderMouse.yDisplayPosition);
-			}
-		}
+	// can't use multiple ModifyArgs because crash ):
+	@ModifyArg(method = "drawScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiContainer;drawItemStack(Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V"), index = 1)
+	private int finement$snapItemRendering$x(int x) {
+		return finement$slotUnderMouse != null ? finement$slotUnderMouse.xDisplayPosition : x;
+	}
+
+	@ModifyArg(method = "drawScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiContainer;drawItemStack(Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V"), index = 2)
+	private int finement$snapItemRendering$y(int y) {
+		return finement$slotUnderMouse != null ? finement$slotUnderMouse.yDisplayPosition : y;
 	}
 }
